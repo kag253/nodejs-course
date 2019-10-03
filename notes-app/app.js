c83a1aa30f4587a62df5936a16f00dcd8fa56c07
Kristian Gennaci <kristian.gennaci@hbo.com>
2019-10-03 12:10:25 -0400

    finished going through the modules section, adding newly created gitignore, packages.json, etc

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,10 @@
+const chalk = require('chalk');
+const validator = require('validator');
 const getNotes = require('./notes.js');
 
 
 console.log(getNotes());
+console.log(validator.isURL('ww..mead.io'));
+
+
+console.log(chalk.red.inverse('Success'));
